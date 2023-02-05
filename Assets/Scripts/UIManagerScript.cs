986f3900af27fd880e9133714202ef94cdafbb84
DR-DRU <negoescu@gmx.de>
2023-02-05 13:15:27 +0100

    Main Menu Updates

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -7,17 +7,33 @@ public class UIManagerScript : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject settingsMenu;
+    public GameObject creditsMenu;
+    public GameObject tutorialMenu;
     // Start is called before the first frame update
     
     public void EnableMainMenu()
     {
         mainMenu.SetActive(true);
         settingsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        tutorialMenu.SetActive(false);
     }
 
     public void EnableSettingsMenu()
     {
         settingsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void EnableTutorialMenu()
+    {
+        tutorialMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void EnableCreditsMenu()
+    {
+        creditsMenu.SetActive(true);
         mainMenu.SetActive(false);
     }
 
